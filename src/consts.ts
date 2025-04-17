32859b2f89f50931f5d7bdb87964b7ac4274f1d1
szkwg <szkwg@hotmail.com>
2025-04-16 23:06:19 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Kuangit";
+export const SITE_DESCRIPTION = "You have power over your mind—not outside events. Realize this, and you will find strength. (Marcus Aurelius)";
